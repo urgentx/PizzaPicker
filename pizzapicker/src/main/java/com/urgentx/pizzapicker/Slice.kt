49e08b9ffa6ed0ea0832b530174e3b48231ae1a4
Barco Zaleta <barcozaleta@gmail.com>
2019-01-24 22:54:24 +0100

    Animate showing text.

--- pizzapicker/src/main/java/com/urgentx/pizzapicker/Slice.kt
+++ pizzapicker/src/main/java/com/urgentx/pizzapicker/Slice.kt
@@ -8,8 +8,6 @@ import android.graphics.Paint
 import android.graphics.RectF
 import android.util.AttributeSet
 import android.view.MotionEvent
-import android.view.View
-import android.view.ViewGroup
 import android.view.animation.*
 import android.widget.RelativeLayout
 import android.widget.TextView
@@ -20,7 +18,6 @@ import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
 import io.reactivex.rxkotlin.addTo
 import io.reactivex.schedulers.Schedulers
-import java.util.*
 import java.util.concurrent.TimeUnit
 import kotlin.math.cos
 import kotlin.math.sin
@@ -34,12 +31,13 @@ class Slice : RelativeLayout {
     private lateinit var currentOval: RectF
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
 
-    private val model: SliceModel
     private var startAngle: Float
     private val originalStartAngle: Float
     private var sweepAngle: Float
     private val originalSweepAngle: Float
     private var margin = 30
+    private var xOffset = 0F
+    private var yOffset = 0F
 
     private val compositeDisposable = CompositeDisposable()
     private var currentAnimDisposable: Disposable? = null
@@ -48,13 +46,18 @@ class Slice : RelativeLayout {
     private var animationProgress = 0F
     private var open = false
 
-    private val textView = TextView(context)
+    private val title = TextView(context)
+    private val text = TextView(context)
 
     init {
         paint.setShadowLayer(12F, 0F, 0F, Color.BLACK)
         setWillNotDraw(false) //enable drawing in this ViewGroup
-        textView.layoutParams = RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT)
-        addView(textView)
+        title.layoutParams = RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT)
+        title.textSize = 25F
+        addView(title)
+        text.layoutParams = RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT)
+        text.alpha = 0F //Initially not seen
+        addView(text)
     }
 
     //TODO: Investigate alternatives for telescoping constructors.
@@ -63,8 +66,8 @@ class Slice : RelativeLayout {
         this.originalStartAngle = startAngle
         this.sweepAngle = sweepAngle
         this.originalSweepAngle = sweepAngle
-        this.model = model
-        textView.text = model.text
+        title.text = model.title
+        text.text = model.text
         paint.color = ContextCompat.getColor(context, model.colorRes)
     }
 
@@ -79,12 +82,14 @@ class Slice : RelativeLayout {
         normalOval = RectF(x + margin, y + margin, w.toFloat() - margin, h.toFloat() - margin)
         //Need to convert angle to radians and offset the position based on the mid angle
         val midAngle = Math.toRadians(((startAngle + (sweepAngle / 2))).toDouble())
-        val xOffset = margin * (cos(midAngle)).toFloat()
-        val yOffset = margin * (sin(midAngle)).toFloat()
+        xOffset = margin * (cos(midAngle)).toFloat()
+        yOffset = margin * (sin(midAngle)).toFloat()
         normalOval.offset(xOffset, yOffset)
         currentOval = RectF(normalOval)
-        textView.x = w / 2 - textView.measuredWidth / 2F + xOffset * 10
-        textView.y = h / 2 - textView.measuredHeight / 2F + yOffset * 10
+        title.x = w / 2 - title.measuredWidth / 2F + xOffset * 10
+        title.y = h / 2 - title.measuredHeight / 2F + yOffset * 10
+        text.x = w / 2 - text.measuredWidth / 2F + xOffset * 10
+        text.y = h / 2 - text.measuredHeight / 2F + yOffset * 10
     }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
@@ -125,8 +130,8 @@ class Slice : RelativeLayout {
                     if (animationProgress in 0F..240F) {
                         val progress = animationProgress / 240F
                         val animationElapsed = interpolator.getInterpolation(if (open) progress else 1 - progress)
-                        if (open) {
-                            sweepAngle = originalSweepAngle + (animationElapsed * 270)
+                        if (open) { //TODO: Refactor into methods.
+                            sweepAngle = originalSweepAngle + (animationElapsed * 270) //TODO: Derive remaining angle till complete
                             startAngle = originalStartAngle - (animationElapsed * 135)
                             val xDiff = (fullOval.centerX() - normalOval.centerX())
                             val yDiff = (fullOval.centerY() - normalOval.centerY())
@@ -134,6 +139,11 @@ class Slice : RelativeLayout {
                             currentOval.bottom = normalOval.bottom + (yDiff + growthRate) * animationElapsed
                             currentOval.left = normalOval.left + (xDiff - growthRate) * animationElapsed
                             currentOval.right = normalOval.right + (xDiff + growthRate) * animationElapsed
+                            text.x = width / 2 - text.measuredWidth / 2F - (xOffset * 15 * animationElapsed) + xOffset * 10
+                            +xOffset * 10
+                            text.y = height / 2 - text.measuredHeight / 2F - (yOffset * 15 * animationElapsed) + yOffset * 10
+                            +yOffset * 10
+                            text.alpha = animationElapsed
                         } else {
                             sweepAngle = originalSweepAngle + 270 - (animationElapsed * 270)
                             startAngle = originalStartAngle - 135 + (animationElapsed * 135)
@@ -143,6 +153,11 @@ class Slice : RelativeLayout {
                             currentOval.bottom = fullOval.bottom - (yDiff + growthRate) * animationElapsed
                             currentOval.left = fullOval.left - (xDiff - growthRate) * animationElapsed
                             currentOval.right = fullOval.right - (xDiff + growthRate) * animationElapsed
+                            text.x = width / 2 - text.measuredWidth / 2F - (xOffset * 15 * (1 - animationElapsed)) + xOffset * 10
+                            +xOffset * 10
+                            text.y = height / 2 - text.measuredHeight / 2F - (yOffset * 15 * (1 - animationElapsed)) + yOffset * 10
+                            +yOffset * 10
+                            text.alpha = 1 - animationElapsed
                         }
                     } else {
                         currentOval = RectF(if (open) fullOval else normalOval)
